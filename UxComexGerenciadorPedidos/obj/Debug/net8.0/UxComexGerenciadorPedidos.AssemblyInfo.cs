8ff2b4028595544534e5b94f3b0bdf1042739009
lpmendesdeoliveira <lpmendesdeoliveira@hotmail.com>
2025-06-07 02:55:49 -0300

    teste

--- UxComexGerenciadorPedidos/obj/Debug/net8.0/UxComexGerenciadorPedidos.AssemblyInfo.cs
+++ UxComexGerenciadorPedidos/obj/Debug/net8.0/UxComexGerenciadorPedidos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UxComexGerenciadorPedidos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0c00309ee97425081bdfdd1037a60dfe1a6fbbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3585207d84992e109be15bfd6a8c11275e91ab54")]
 [assembly: System.Reflection.AssemblyProductAttribute("UxComexGerenciadorPedidos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UxComexGerenciadorPedidos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
